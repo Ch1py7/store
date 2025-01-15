1230ac54c6dfb6a67d33a47287ec228b397963ba
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-01-14 22:13:03 -0600

    feat: add Account page with client dashboard and user information form; update routing in App component

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import { Layout } from './app/layout/Layout'
 import { ProductsCatalog as Products } from './pages/Products/ProductsCatalog'
 import { SearchBar } from '@/shared/ui/SearchBar'
+import { Account } from './pages/Account/Account'
 
 export const App = () => {
 	return (
@@ -10,6 +11,7 @@ export const App = () => {
 			<Layout>
 				<Routes>
 					<Route path='/' element={<Products />} />
+					<Route path='/account' element={<Account />} />
 				</Routes>
 			</Layout>
 		</Router>
---
+++ apps/frontend/src/pages/Account/Account.tsx
@@ -0,0 +1,7 @@
+import { Client } from './Client'
+
+export const Account: React.FC = (): React.ReactNode => {
+  return (
+    <Client />
+  )
+}
