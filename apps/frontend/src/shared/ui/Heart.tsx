29f0b8130525fdb0186f7e3eca22c6e202671666
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-01-14 22:11:00 -0600

    feat: add optional classNames prop to Heart component for customizable styling

--- apps/frontend/src/shared/ui/Heart.tsx
+++ apps/frontend/src/shared/ui/Heart.tsx
@@ -1,8 +1,9 @@
 interface HeartProps {
-	like: boolean
+	like?: boolean
+	classNames?: string
 }
 
-export const Heart: React.FC<HeartProps> = ({ like }): React.ReactNode => {
+export const Heart: React.FC<HeartProps> = ({ like, classNames }): React.ReactNode => {
 	return (
 		<>
 			<svg
@@ -15,7 +16,7 @@ export const Heart: React.FC<HeartProps> = ({ like }): React.ReactNode => {
 				strokeWidth='2'
 				strokeLinecap='round'
 				strokeLinejoin='round'
-				className='lucide lucide-heart'
+				className={`lucide lucide-heart ${classNames}`}
 			>
 				<title>Fav</title>
 				<path d='M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z' />
