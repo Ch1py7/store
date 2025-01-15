adf32e8f8a3a295b015e22be029b933e3435e891
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-01-14 18:56:40 -0600

    feat: integrate ProductsCatalog and SearchBar into the main App component

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Layout } from './app/layout/Layout'
+import { ProductsCatalog as Products } from './pages/Products/ProductsCatalog'
+import { SearchBar } from '@/shared/ui/SearchBar'
 
 export const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<></>} />
-      </Routes>
-    </Router>
-  )
+	return (
+		<Router>
+			<SearchBar />
+			<Layout>
+				<Routes>
+					<Route path='/' element={<Products />} />
+				</Routes>
+			</Layout>
+		</Router>
+	)
 }
