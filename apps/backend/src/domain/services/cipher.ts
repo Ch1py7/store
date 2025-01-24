df237e224975e52510fe4ac655fc618ec2bc5748
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-01-24 17:22:20 -0600

    feat: simplify CryptoCipher by removing encryption and decryption methods

--- apps/backend/src/domain/services/cipher.ts
+++ apps/backend/src/domain/services/cipher.ts
@@ -1,8 +1,5 @@
 export interface Cipher {
-	encrypt(value: string): { content: string; iv: string }
-	decrypt(encryption: { content: string; iv: string }): string
 	randomUUID(): string
-	randomUUID(value: string): string
 	randomString(length: number): string
 	hashPassword(password: string): { hashedPassword: string; salt: string }
 	verifyPassword(storedHashBase64: string, storedSalt: string, password: string): boolean
