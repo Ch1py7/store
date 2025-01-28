9a32c6f8e9bc907c57c9d984fb40aee33a63c620
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-01-28 16:07:55 -0600

    feat: implement user login functionality with JWT session creation

--- apps/auth_api/src/infrastructure/http/auth_controller.ts
+++ apps/auth_api/src/infrastructure/http/auth_controller.ts
@@ -1,3 +1,5 @@
+import { CreateSessionCommand } from '@/application/create_session/command'
+import { LoginCommand } from '@/application/login/command'
 import { CreateCommand } from '@/application/register/command'
 import { container } from '@/container'
 import express, { type Router } from 'express'
@@ -15,19 +17,56 @@ router.post('/auth/register', async (req: express.Request, res: express.Response
 	}
 
 	try {
-		const command = new CreateCommand({ firstName, email, lastName, password, role })
+		const userCommand = new CreateCommand({ firstName, email, lastName, password, role })
 		const registerUser = container.resolve('registerUser')
-		const response = await registerUser.execute(command)
+		const userResponse = await registerUser.execute(userCommand)
+
+		const sessionCommand = new CreateSessionCommand(userResponse)
+		const createSession = container.resolve('createSession')
+		const { jwt } = await createSession.execute(sessionCommand)
 
 		res.status(201).json({
 			message: 'User registered successfully',
-			data: response,
+			data: jwt,
 		})
 	} catch (error) {
 		console.error('Error registering user:', error)
 
 		res.status(500).json({
 			message: 'An error occurred while register the user',
+			error: (error as Error).message || 'Unknown error',
+		})
+	}
+})
+
+router.get('/auth/login', async (req: express.Request, res: express.Response) => {
+	const { email, password } = req.query as { email: string, password: string}
+
+	if (!email || !password) {
+		res.status(400).json({
+			message: 'Missing required fields: email, password',
+		})
+		return
+	}
+
+	try {
+		const userCommand = new LoginCommand({ email, password })
+		const loginUser = container.resolve('loginUser')
+		const userResponse = await loginUser.execute(userCommand)
+
+		const sessionCommand = new CreateSessionCommand(userResponse)
+		const createSession = container.resolve('createSession')
+		const { jwt } = await createSession.execute(sessionCommand)
+
+		res.status(200).json({
+			message: 'User login successfully',
+			data: jwt,
+		})
+	} catch (error) {
+		console.error('Error login user:', error)
+
+		res.status(500).json({
+			message: 'An error occurred while login the user',
 			error: (error as Error).message || 'Unknown error',
 		})
 	}
