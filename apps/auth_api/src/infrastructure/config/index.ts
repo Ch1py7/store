4da519783574579e487df2cbbee49f5af3e32358
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-01-28 16:07:24 -0600

    feat: add jwtSecret to configuration for JWT signing

--- apps/auth_api/src/infrastructure/config/index.ts
+++ apps/auth_api/src/infrastructure/config/index.ts
@@ -3,6 +3,7 @@ dotenv.config()
 
 export const config = {
 	privateKey: process.env.PRIVATE_KEY ?? '',
+	jwtSecret: process.env.JWT_SECRET ?? '',
 	supabase: {
 		key: process.env.SUPABASE_KEY!,
 		url: process.env.SUPABASE_URL!,
