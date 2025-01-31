aa59d642a4f2af135ec378000c721010eef8c812
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-01-31 17:52:26 -0600

    feat: add findAll and findById methods to OrderRepository for enhanced order retrieval

--- apps/backend/src/domain/repositories/order-repository.ts
+++ apps/backend/src/domain/repositories/order-repository.ts
@@ -1,8 +1,9 @@
 import type { Database, Order as OrderDomain } from '@store/core'
 
 export interface IOrderRepository {
+	findAll(): Promise<OrderDomain[] | null>
+	findById(id: string): Promise<OrderDomain>
 	findOrdersByUserId(id: string): Promise<OrderDomain[]>
-	findOrderById(id: string): Promise<OrderDomain>
 	saveOrderUpdateProduct(
 		order: OrderDomain,
 		product: Pick<Database['public']['Tables']['Product']['Row'], 'id' | 'stock' | 'updated_at'>[]
--- apps/backend/src/infrastructure/repositories/order/order-repository.ts
+++ apps/backend/src/infrastructure/repositories/order/order-repository.ts
@@ -16,7 +16,6 @@ export class OrderRepository implements IOrderRepository {
 		order: Order,
 		products: Pick<Database['public']['Tables']['Product']['Row'], 'id' | 'updated_at'>[]
 	) {
-		console.log('products', products)
 		const { error } = await this._supabaseClient.rpc('update_product_from_order', {
 			order_table_data: this._orderParser.toDbModel(order),
 			product_table_data: products,
@@ -26,24 +25,33 @@ export class OrderRepository implements IOrderRepository {
 	}
 
 	async findOrdersByUserId(id: string) {
-		const { data, error } = await this._supabaseClient.from('Order').select('*').eq('user_id', id)
-
-		if (error) throw error
-
-		const orders = data.map((db) => this._orderParser.toDomain(db))
-
-		return orders
-	}
-
-	async findOrderById(id: string) {
 		const { data, error } = await this._supabaseClient
 			.from('Order')
 			.select('*')
 			.eq('user_id', id)
+
+		if (error) throw error
+
+		return data.map((db) => this._orderParser.toDomain(db))
+	}
+
+	async findById(id: string) {
+		const { data, error } = await this._supabaseClient
+			.from('Order')
+			.select('*')
+			.eq('id', id)
 			.single()
 
 		if (error) throw error
 
 		return this._orderParser.toDomain(data)
+	}
+
+	async findAll() {
+		const { data, error } = await this._supabaseClient.from('Order').select('*')
+
+		if (error) throw error
+
+		return data.map((db) => this._orderParser.toDomain(db))
 	}
 }
