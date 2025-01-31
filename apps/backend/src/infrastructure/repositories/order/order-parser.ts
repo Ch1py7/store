806f11712dc01171d6c79c9fbe7edbd177b77c35
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-01-30 22:02:19 -0600

    feat: enhance OrderParser to handle products with ProductParser and improve JSON parsing

--- apps/backend/src/infrastructure/repositories/order/order-parser.ts
+++ apps/backend/src/infrastructure/repositories/order/order-parser.ts
@@ -1,24 +1,24 @@
 import { Order as OrderDomain, type Database } from '@store/core'
+import { ProductParser } from '../product/parser/product-parser'
 
 export class OrderParser {
-	toDomain(dbModel: Database['public']['Tables']['Order']['Row']): OrderDomain {
+	public toDomain(dbModel: Database['public']['Tables']['Order']['Row']): OrderDomain {
 		return new OrderDomain({
 			id: dbModel.id,
 			updatedAt: new Date(dbModel.updated_at).getTime(),
 			createdAt: new Date(dbModel.created_at).getTime(),
-			products: JSON.parse(dbModel.products),
+			products: JSON.parse(dbModel.products?.toString() ?? '[]'),
 			status: dbModel.status,
-			total: dbModel.total,
 			userId: dbModel.user_id,
 		})
 	}
 
-	toDbModel(domainModel: OrderDomain): Database['public']['Tables']['Order']['Row'] {
+	public toDbModel(domainModel: OrderDomain): Database['public']['Tables']['Order']['Row'] {
 		return {
 			id: domainModel.id,
 			updated_at: new Date(domainModel.updatedAt).toISOString(),
 			created_at: new Date(domainModel.createdAt).toISOString(),
-			products: JSON.stringify(domainModel.products),
+			products: domainModel.products.map((product) => new ProductParser().toDbModel(product)),
 			status: domainModel.status,
 			total: domainModel.total,
 			user_id: domainModel.userId,
