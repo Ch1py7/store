6b60f9373a3dbcfd48eaef5f9e9627b56945d0b6
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-02-02 13:41:02 -0600

    feat: add CartParser for converting between domain and database models

---
+++ apps/backend/src/infrastructure/repositories/cart/cart-parser.ts
@@ -0,0 +1,24 @@
+import { Cart as CartDomain, type Database } from '@store/core'
+
+export class CartParser {
+	public toDomain(dbModel: Database['public']['Tables']['Cart']['Row']): CartDomain {
+		return new CartDomain({
+			id: dbModel.id,
+			updatedAt: new Date(dbModel.updated_at).getTime(),
+			createdAt: new Date(dbModel.created_at).getTime(),
+			products: JSON.parse(JSON.stringify(dbModel.products)),
+			userId: dbModel.user_id
+		})
+	}
+
+	public toDbModel(domainModel: CartDomain): Database['public']['Tables']['Cart']['Row'] {
+		return {
+			id: domainModel.id,
+			updated_at: new Date(domainModel.updatedAt).toISOString(),
+			created_at: new Date(domainModel.createdAt).toISOString(),
+			products: JSON.parse(JSON.stringify(domainModel.products)),
+			total: domainModel.total,
+			user_id: domainModel.userId,
+		}
+	}
+}
