38568085e9f10777047849274946bd82fc9aba01
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-02-02 15:12:59 -0600

    feat: implement AMQP client for message publishing and consumption with configuration updates

--- apps/backend/src/infrastructure/config/index.ts
+++ apps/backend/src/infrastructure/config/index.ts
@@ -3,13 +3,14 @@ dotenv.config()
 
 export const config = {
 	privateKey: process.env.PRIVATE_KEY ?? '',
+	jwtSecret: process.env.JWT_SECRET ?? '',
 	supabase: {
 		key: process.env.SUPABASE_KEY!,
 		url: process.env.SUPABASE_URL!,
 	},
 	server: {
-		port: process.env.PORT ?? 3001,
-		amqpUrl: process.env.AMQP_URL,
-		cipherKey: process.env.CIPHER_KEY,
+		port: process.env.PORT ?? 7777,
+		amqpUrl: process.env.AMQP_URL ?? '',
+		cipherKey: process.env.CIPHER_KEY ?? '',
 	},
 }
