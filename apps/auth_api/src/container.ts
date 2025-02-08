9e3e4fee0b2387176e893d76ba5e03fab7eb91b3
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-02-07 22:37:47 -0600

    feat: register RefreshSession and TokensRepository in the dependency container

--- apps/auth_api/src/container.ts
+++ apps/auth_api/src/container.ts
@@ -3,14 +3,17 @@ import { InjectionMode, asClass, asValue, createContainer } from 'awilix'
 import crypto from 'node:crypto'
 import { CreateSession } from './application/create_session'
 import { LoginUser } from './application/login'
+import { RefreshSessionSession } from './application/refresh_session'
 import { RegisterUser } from './application/register'
 import { config } from './infrastructure/config'
 import { PubSubClient } from './infrastructure/pubsub/pubsub-client'
 import { AuthParser } from './infrastructure/repositories/auth/auth-parser'
 import { UserAuthRepository } from './infrastructure/repositories/auth/user-auth-repository'
+import { CartRepository } from './infrastructure/repositories/cart/cart-repository'
+import { RefreshTokenParser } from './infrastructure/repositories/tokens/tokens-parser'
+import { TokensRepository } from './infrastructure/repositories/tokens/tokens-repository'
 import { CryptoCipher } from './infrastructure/security/crypto-cypher'
 import { supabaseClient } from './infrastructure/supabase/client'
-import { CartRepository } from './infrastructure/repositories/cart/cart-repository'
 
 export const container = createContainer<Dependencies>({
 	injectionMode: InjectionMode.PROXY,
@@ -25,6 +28,7 @@ container.register({
 	registerUser: asClass(RegisterUser),
 	loginUser: asClass(LoginUser),
 	createSession: asClass(CreateSession),
+	refreshSession: asClass(RefreshSessionSession),
 
 	// DB
 	supabaseClient: asValue(supabaseClient),
@@ -32,6 +36,7 @@ container.register({
 	// Repositories
 	userAuthRepository: asClass(UserAuthRepository),
 	cartRepository: asClass(CartRepository),
+	tokensRepository: asClass(TokensRepository),
 
 	// PubSub
 	pubSubClient: asClass(PubSubClient),
@@ -39,4 +44,5 @@ container.register({
 	// Parser
 	userParser: asClass(UserParser),
 	authParser: asClass(AuthParser),
+	refreshTokensParser: asClass(RefreshTokenParser),
 })
