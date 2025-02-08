b75a923d94db62da10ccb41d43882214dcac2ff4
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-02-07 22:31:46 -0600

    feat: enhance CreateSession to manage refresh tokens and revoke old tokens

--- apps/auth_api/src/application/create_session/index.ts
+++ apps/auth_api/src/application/create_session/index.ts
@@ -1,34 +1,44 @@
+import { RefreshToken } from '@/domain/refresh_tokens/refresh-tokens'
 import type { CreateSessionCommand } from './command'
 import { CreateSessionResponse } from './response'
 
 export class CreateSession {
 	private _cipher: Dependencies['cipher']
+	private _tokensRepository: Dependencies['tokensRepository']
 
-	constructor({ cipher }: Pick<Dependencies, 'cipher'>) {
+	constructor({ cipher, tokensRepository }: Pick<Dependencies, 'cipher' | 'tokensRepository'>) {
 		this._cipher = cipher
+		this._tokensRepository = tokensRepository
 	}
 
-	public async execute({ firstName, lastName, role, id: userId }: CreateSessionCommand) {
-		const access_token = this._cipher.signJwt(
-			{
-				firstName,
-				lastName,
-				role,
-				userId,
-			},
-			false
-		)
+	public async execute({ firstName, lastName, role, id }: CreateSessionCommand) {
+		const accessToken_iat = Date.now()
+		const accessToken_exp = accessToken_iat + 900
+		const access_token = this._cipher.signJwt({
+			firstName,
+			lastName,
+			role,
+			sub: id,
+			iat: accessToken_iat,
+			exp: accessToken_exp,
+		})
 
-		const refresh_token = this._cipher.signJwt(
-			{
-				firstName,
-				lastName,
-				role,
-				userId,
-			},
-			true
-		)
+		const refresh_token = this._cipher.randomString(64)
 
-		return new CreateSessionResponse({ access_token, refresh_token })
+		const token = new RefreshToken({
+			created_at: Date.now(),
+			id: this._cipher.randomUUID(),
+			isRevoked: false,
+			refreshToken: refresh_token,
+			userId: id,
+		})
+
+		await this._tokensRepository.revokeTokens(id)
+		await this._tokensRepository.save(token)
+
+		return new CreateSessionResponse({
+			access_token,
+			refresh_token,
+		})
 	}
 }
