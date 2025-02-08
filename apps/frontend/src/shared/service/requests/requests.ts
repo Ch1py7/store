8aadd8db7ae0e2fba996b60b5b8ae9e2e4ec38ea
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-02-08 03:43:56 -0600

    feat: implement axios instance with response interceptors for token refresh and error handling

--- apps/frontend/src/shared/service/requests/requests.ts
+++ apps/frontend/src/shared/service/requests/requests.ts
@@ -1,11 +1,65 @@
 import axios, { AxiosError } from 'axios'
+import { AuthService } from './auth'
 
-export const postRequest = async <T>(url: string, data: any, token?: string) => {
-	try {
-		const config = {
-			headers: { Authorization: `Bearer ${token}` },
+const axiosInstance = axios.create({
+	withCredentials: true,
+})
+
+axiosInstance.interceptors.response.use(
+	(response) => response,
+	async (error) => {
+		const originalRequest = error.config
+		const { error: errorMessage } = error.response.data
+
+		if (
+			error.response?.status === 401 &&
+			!originalRequest._retry &&
+			errorMessage === 'Unauthorized'
+		) {
+			originalRequest._retry = true
+
+			try {
+				await axiosInstance.post(AuthService.refreshToken)
+
+				return axiosInstance(originalRequest)
+			} catch (refreshError) {
+				logoutAndRedirect()
+				return Promise.reject(refreshError)
+			}
 		}
-		const response = (await axios.post(url, data, config)) as { data: T; status: number }
+
+		if (error.response?.status === 500 && errorMessage === 'Refresh token revoked') {
+			logoutAndRedirect()
+			return Promise.reject(error)
+		}
+
+		return Promise.reject(error)
+	}
+)
+
+const logoutAndRedirect = () => {
+	// window.location.href = '/auth/login'
+}
+
+export const postRequest = async <T>(url: string, dataToSend: any) => {
+	try {
+		const { data, status } = (await axiosInstance.post(url, dataToSend)) as {
+			data: T
+			status: number
+		}
+
+		return { data, status }
+	} catch (er) {
+		if (er instanceof AxiosError) {
+			throw er
+		}
+		throw [(er as Error).message]
+	}
+}
+
+export const getRequest = async <T>(url: string) => {
+	try {
+		const response = (await axiosInstance.get(url)) as { data: T; status: number }
 
 		return { data: response.data, status: response.status }
 	} catch (er) {
@@ -16,12 +70,9 @@ export const postRequest = async <T>(url: string, data: any, token?: string) => 
 	}
 }
 
-export const getRequest = async <T>(url: string, token?: string) => {
+export const deleteRequest = async <T>(url: string) => {
 	try {
-		const config = {
-			headers: { Authorization: `Bearer ${token ? token : ''}` },
-		}
-		const response = (await axios.get(url, config)) as { data: T; status: number }
+		const response = (await axiosInstance.delete(url)) as { data: T; status: number }
 
 		return { data: response.data, status: response.status }
 	} catch (er) {
@@ -32,28 +83,9 @@ export const getRequest = async <T>(url: string, token?: string) => {
 	}
 }
 
-export const deleteRequest = async <T>(url: string, token: string) => {
+export const patchRequest = async <T>(url: string, data: T) => {
 	try {
-		const config = {
-			headers: { Authorization: `Bearer ${token}` },
-		}
-		const response = (await axios.delete(url, config)) as { data: T; status: number }
-
-		return { data: response.data, status: response.status }
-	} catch (er) {
-		if (er instanceof AxiosError) {
-			throw er
-		}
-		throw [(er as Error).message]
-	}
-}
-
-export const patchRequest = async <T>(url: string, data: T, token?: string) => {
-	try {
-		const config = {
-			headers: { Authorization: `Bearer ${token}` },
-		}
-		const response = (await axios.patch(url, data, config)) as { data: T; status: number }
+		const response = (await axiosInstance.patch(url, data)) as { data: T; status: number }
 
 		return { data: response.data, status: response.status }
 	} catch (er) {
