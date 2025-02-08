28f9bfc5aba9c8f36747e63457efbf111c86cd0a
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-02-08 02:35:01 -0600

    feat: enhance CORS configuration and add cookie parser middleware

--- apps/auth_api/src/index.ts
+++ apps/auth_api/src/index.ts
@@ -1,13 +1,20 @@
-import 'module-alias/register'
 import cors from 'cors'
 import express from 'express'
+import 'module-alias/register'
 import { router as authRoutes } from './infrastructure/http/auth_controller'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 const port = process.env.PORT || 7777
 
 app.use(express.json())
-app.use(cors({ origin: '*' }))
+app.use(cookieParser())
+app.use(
+	cors({
+		origin: ['http://localhost:5173', 'http://localhost:7777'],
+		credentials: true,
+	})
+)
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/v1/', authRoutes)
