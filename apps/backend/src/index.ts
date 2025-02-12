1bd769b74f7f5164e0cae8bb278fe9f507b237dd
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-02-11 23:33:32 -0600

    feat: add cart routes and integrate cookie-parser for improved session handling

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -4,6 +4,8 @@ import express from 'express'
 import { router as userRoutes } from './infrastructure/http/user-controller'
 import { router as productRoutes } from './infrastructure/http/product-controller'
 import { router as orderRoutes } from './infrastructure/http/order-controller'
+import { router as CartRoutes } from './infrastructure/http/cart-controller'
+import cookieParser from 'cookie-parser'
 import { container } from './container'
 
 const listener = container.resolve('pubSubListener')
@@ -13,12 +15,19 @@ const app = express()
 const port = process.env.PORT || 7777
 
 app.use(express.json())
-app.use(cors({ origin: '*' }))
+app.use(cookieParser())
+app.use(
+	cors({
+		origin: ['http://localhost:5173', 'http://localhost:7777'],
+		credentials: true,
+	})
+)
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/v1/', userRoutes)
 app.use('/api/v1/', productRoutes)
 app.use('/api/v1/', orderRoutes)
+app.use('/api/v1/', CartRoutes)
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`)
