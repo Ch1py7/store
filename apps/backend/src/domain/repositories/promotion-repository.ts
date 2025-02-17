34af81bddaaa26fd0489bac59632ba1e67c770af
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-02-17 01:55:15 -0600

    feat: add IPromotionRepository interface for managing user carts

---
+++ apps/backend/src/domain/repositories/promotion-repository.ts
@@ -0,0 +1,7 @@
+import type { Cart as CartDomain } from '@store/core'
+
+export interface IPromotionRepository {
+	findByUserId(id: string): Promise<CartDomain>
+	createCart(cart: CartDomain): Promise<void>
+	updateCart(cart: CartDomain): Promise<void>
+}
